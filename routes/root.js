bde34ea5d4574f9655f2a9f508258a25857862fe
LAGC24 <lagc2412@gmail.com>
2016-02-25 18:07:36 -0600

    WS-11.0.3 <LAGC24@LAGC24-PC Overwrite local to https://github.com/LAGC24/comidapp

--- routes/root.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Edmundo on 2/24/2016.
- */
-
-exports.root = function(req, res) {
-  //resp.sendFile(__dirname + '/app/index.html');
-  res.sendFile(req.app.get('viewsDir') + '/index.html');
-};
